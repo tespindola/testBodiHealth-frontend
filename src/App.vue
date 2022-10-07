82e43ba46d781892304f4aadd23ed275d66df2f3
tespindola <t.tombits@gmail.com>
2022-10-07 11:24:53 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h1 class="text-xl text-red-500">GOD</h1>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
